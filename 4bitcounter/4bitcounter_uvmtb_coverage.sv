fb518cec62342f50fa36cf81538e515f93e396a0
thinhngo11 <35941824+thinhngo11@users.noreply.github.com>
2023-05-04 13:59:34 +0700

    Update 4bitcounter_uvmtb_coverage.sv

--- 4bitcounter/4bitcounter_uvmtb_coverage.sv
+++ 4bitcounter/4bitcounter_uvmtb_coverage.sv
@@ -1,3 +1,6 @@
+1. Create a coverage class:
+
+```systemverilog
 class counter_coverage;
     // Covergroup
     covergroup cg_counter(string name) @(posedge clk);
@@ -29,5 +32,72 @@ class counter_coverage;
         cg.up_down = up_down;
         cg.load_data = load_data;
         cg.sample();
+    endfunction
+endclass
+```
+
+2. Instantiate the coverage class in the `counter_monitor`:
+
+```systemverilog
+class counter_monitor extends uvm_monitor;
+    // ...
+
+    // Coverage instance
+    counter_coverage coverage;
+
+    // Constructor
+    function new(string name = "counter_monitor", uvm_component parent);
+        super.new(name, parent);
+    endfunction
+
+    // Build phase
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+
+        ap = new("ap", this);
+        coverage = new();
+    endfunction
+
+    // Main task
+    task main_phase(uvm_phase phase);
+        counter_transaction tr;
+
+        forever begin
+            @(posedge intf.clk);
+            tr = counter_transaction::type_id::create("tr");
+            tr.reset = intf.reset;
+            tr.load = intf.load;
+            tr.up_down = intf.up_down;
+            tr.load_data = intf.load_data;
+            tr.count = intf.count;
+
+            // Sample coverage
+            coverage.sample(tr.reset, tr.load, tr.up_down, tr.load_data);
+
+            ap.write(tr);
+        end
+    endtask
+endclass
+```
+
+3. Add a coverage callback in the `counter_test`:
+
+```systemverilog
+class counter_test extends uvm_test;
+    // ...
+
+    // Main phase
+    task main_phase(uvm_phase phase);
+        // ...
+
+        // Register for end_of_simulation_phase callback
+        phase.phase_done.set_end_of_simulation_phase(end_of_simulation_phase);
+    endtask
+
+    // End of simulation phase
+    function void end_of_simulation_phase(uvm_phase phase);
+        // Print coverage report
+        $display("Coverage Report:");
+        $display("[COVERAGE] %s", env.agent.monitor.coverage.cg.get_coverage_report());
     endfunction
 endclass
