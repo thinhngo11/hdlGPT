dde457372512f5d11a3e7a36c7dd289a65aebde5
thinhngo11 <35941824+thinhngo11@users.noreply.github.com>
2023-05-04 18:57:31 +0700

    Update FIFO_uvmtb_env.sv

--- FIFO/FIFO_uvmtb_env.sv
+++ FIFO/FIFO_uvmtb_env.sv
@@ -4,6 +4,8 @@ class fifo_env extends uvm_env;
     `uvm_component_utils(fifo_env)
 
     fifo_agent agent;
+    fifo_monitor monitor;
+    fifo_scoreboard scoreboard;
 
     function new(string name, uvm_component parent);
         super.new(name, parent);
@@ -11,5 +13,11 @@ class fifo_env extends uvm_env;
 
     virtual function void build_phase(uvm_phase phase);
         agent = fifo_agent::type_id::create("agent", this);
+        monitor = fifo_monitor::type_id::create("monitor", this);
+        scoreboard = fifo_scoreboard::type_id::create("scoreboard", this);
+    endfunction
+
+    virtual function void connect_phase(uvm_phase phase);
+        monitor.ap.connect(scoreboard.fifo_fifo.analysis_export);
     endfunction
 endclass
