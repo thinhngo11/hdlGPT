84c0e58e0b6a2f3ae2e95bd99f2ea0f4fed1e5df
thinhngo11 <35941824+thinhngo11@users.noreply.github.com>
2023-05-04 17:59:50 +0700

    Create LFSR_uvmtb_env.sv

---
+++ randomnumber/LFSR_uvmtb_env.sv
@@ -0,0 +1,25 @@
+class rng_env extends uvm_env;
+    `uvm_component_utils(rng_env)
+
+    rng_agent agent;
+
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction
+
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        agent = rng_agent::type_id::create("agent", this);
+    endfunction
+
+    function void connect_phase(uvm_phase phase);
+        uvm_config_db#(rng_if)::set(this, "agent", "rng_if", get_interface_handle());
+    endfunction
+
+    protected function rng_if get_interface_handle();
+        rng_if rng_if;
+        if (!uvm_config_db#(rng_if)::get(this, "", "rng_if", rng_if))
+            `uvm_fatal("RNG_ENV", "Did not get the rng_if handle")
+        return rng_if;
+    endfunction
+endclass
