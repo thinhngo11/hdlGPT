f81fb8a11e1fd1398dd03f598a6ac8479465aec5
thinhngo11 <35941824+thinhngo11@users.noreply.github.com>
2023-05-04 13:24:40 +0700

    Update demux1to8_uvmtb_coverage.sv

--- demux1to8/demux1to8_uvmtb_coverage.sv
+++ demux1to8/demux1to8_uvmtb_coverage.sv
@@ -1,4 +1,7 @@
 class demux_coverage_collector extends uvm_component;
+  
+  uvm_analysis_port #(demux_seq_item) ap;
+  
   covergroup demux_cg;
     coverpoint sel_cp;
     coverpoint data_in_cp;
@@ -6,8 +9,17 @@ class demux_coverage_collector extends uvm_component;
 
   function new(string name, uvm_component parent);
     super.new(name, parent);
+    ap = new("ap", this);
   endfunction
 
+  function void write_demux_seq_item(demux_seq_item item);
+    sample(item.sel, item.data_in);
+  endfunction
+
+  function void connect_phase(uvm_phase phase);
+    ap.connect_analysis_export(this, "write_demux_seq_item");
+  endfunction
+  
   function void build_phase(uvm_phase phase);
     demux_cg = new();
   endfunction
