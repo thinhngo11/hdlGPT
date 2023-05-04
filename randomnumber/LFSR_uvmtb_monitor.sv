8e2285bf70c3937a009b82d27c13f365e2cfc6e8
thinhngo11 <35941824+thinhngo11@users.noreply.github.com>
2023-05-04 19:35:44 +0700

    Update LFSR_uvmtb_monitor.sv

--- randomnumber/LFSR_uvmtb_monitor.sv
+++ randomnumber/LFSR_uvmtb_monitor.sv
@@ -1,5 +1,10 @@
 class rng_monitor extends uvm_monitor;
     `uvm_component_utils(rng_monitor)
+    
+    // Declaration
+    rand bit [7:0] rng_out;
+    rand bit [7:0] rng_out_prev;
+    rng_cg rng_cg_inst;
 
     rng_if rng_if;
     uvm_analysis_port#(logic[7:0]) item_port;
@@ -7,6 +12,7 @@ class rng_monitor extends uvm_monitor;
     function new(string name, uvm_component parent);
         super.new(name, parent);
         item_port = new("item_port", this);
+        rng_cg_inst = new();
     endfunction
 
     function void build_phase(uvm_phase phase);
@@ -16,6 +22,9 @@ class rng_monitor extends uvm_monitor;
 
     virtual task run_phase(uvm_phase phase);
         forever @(posedge rng_if.clk) begin
+            rng_out_prev = rng_out;
+            rng_out = dut_inst.rng_out;
+            rng_cg_inst.sample();
             item_port.write(rng_if.random_number);
         end
     endtask
