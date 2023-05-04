3abf737938d4e2d63f5549e56791b6fe8ad5777e
thinhngo11 <35941824+thinhngo11@users.noreply.github.com>
2023-05-04 19:12:28 +0700

    Update FIFO_uvmtb_monitor.sv

--- FIFO/FIFO_uvmtb_monitor.sv
+++ FIFO/FIFO_uvmtb_monitor.sv
@@ -5,10 +5,12 @@ class fifo_monitor extends uvm_monitor;
 
     virtual interface fifo_if vif;
     uvm_analysis_port #(fifo_transaction) ap;
+    fifo_covergroup fifo_cov;
 
     function new(string name, uvm_component parent);
         super.new(name, parent);
         ap = new("ap", this);
+        fifo_cov = new();
     endfunction
 
     virtual task run_phase(uvm_phase phase);
@@ -21,12 +23,14 @@ class fifo_monitor extends uvm_monitor;
                 trans.trans_type = fifo_trans_type_e.WRITE;
                 trans.data = vif.wr_data;
                 ap.write(trans);
+                fifo_cov.sample_trans(trans);
             end
 
             if (vif.rd_en && !vif.empty) begin
                 trans.trans_type = fifo_trans_type_e.READ;
                 trans.data = vif.rd_data;
                 ap.write(trans);
+                fifo_cov.sample_trans(trans);
             end
         end
     endtask
