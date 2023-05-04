988791c276f9ebdcd6718a4d229ca43e63b63d1e
thinhngo11 <35941824+thinhngo11@users.noreply.github.com>
2023-05-04 18:20:28 +0700

    Update LFSR_uvmtb_top.sv

--- randomnumber/LFSR_uvmtb_top.sv
+++ randomnumber/LFSR_uvmtb_top.sv
@@ -12,9 +12,9 @@ module tb_top;
         clk = 0;
         forever #5 clk = ~clk;
     end
-
+    
     initial begin
-        uvm_config_db#(rng_if)::set(null, "uvm_test_top", "rng_if", rng_if_inst);
+        uvm_config_db#(uvm_sequencer#(rng_seq_item))::set(null, "uvm_test_top.env.agent.seqr", "default_sequence", rng_seq::type_id::get());
         run_test("rng_test");
     end
 endmodule
