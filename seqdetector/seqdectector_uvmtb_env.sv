6c001b483387cb7c5d531c5ba9791e2fc1fc7280
thinhngo11 <35941824+thinhngo11@users.noreply.github.com>
2023-05-05 15:36:27 +0700

    Update seqdectector_uvmtb_env.sv

--- seqdetector/seqdectector_uvmtb_env.sv
+++ seqdetector/seqdectector_uvmtb_env.sv
@@ -4,6 +4,8 @@ class env extends uvm_env;
 
   agent agt;
   scoreboard sb;  
+  coverage cov;
+  
   virtual sequence_detector_if vif;
 
   function new(string name, uvm_component parent);
@@ -14,10 +16,12 @@ class env extends uvm_env;
     super.build_phase(phase);
     agt = agent::type_id::create("agt", this);
     sb = scoreboard::type_id::create("sb", this);    
+    cov = coverage::type_id::create("cov", this);    
   endfunction
 
   function void connect_phase(uvm_phase phase);
     agt.vif = vif;
     sb.exp.connect(agt.mon.ap);    
+    agt.mon.ap.connect(cov.analysis_export);    
   endfunction
 endclass
