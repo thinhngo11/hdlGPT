8902a37cc1391fb73ac30b786019182db72d9c4c
thinhngo11 <35941824+thinhngo11@users.noreply.github.com>
2023-05-05 15:49:26 +0700

    Create trafficlight_uvmtb_tb.sv

---
+++ trafficlight/trafficlight_uvmtb_tb.sv
@@ -0,0 +1,34 @@
+`include "uvm_macros.svh"
+
+module traffic_light_controller_tb;
+  reg clk;
+  wire reset;
+  wire [1:0] north_south;
+  wire [1:0] east_west;
+
+  traffic_light_controller_if intf(.clk(clk));
+
+  // Instantiate the 4-way traffic light controller
+  traffic_light_controller uut (
+    .clk(clk),
+    .reset(reset),
+    .north_south(north_south),
+    .east_west(east_west)
+  );
+
+  // Clock generation
+  always begin
+    #5 clk = ~clk;
+  end
+
+  initial begin
+    // Initialize signals
+    clk = 0;
+    intf.reset = 1;
+
+    // Run the UVM test
+    uvm_config_db#(virtual traffic_light_controller_if)::set(null, "uvm_test_top.env.agent", "intf", intf);
+    run_test("traffic_light_controller_test");
+  end
+
+endmodule
