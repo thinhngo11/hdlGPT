f1fcf8caf62462750808d6058e50d22782fc6eaf
thinhngo11 <35941824+thinhngo11@users.noreply.github.com>
2023-05-05 16:17:19 +0700

    Update comparator_uvmtb_env.sv

--- 4bitcomparator/comparator_uvmtb_env.sv
+++ 4bitcomparator/comparator_uvmtb_env.sv
@@ -2,6 +2,7 @@ class mc_env extends uvm_env;
     `uvm_component_utils(mc_env)
 
     mc_agent agent;
+    mc_scoreboard scoreboard;
 
     function new(string name, uvm_component parent);
         super.new(name, parent);
@@ -10,9 +11,11 @@ class mc_env extends uvm_env;
     virtual function void build_phase(uvm_phase phase);
         super.build_phase(phase);
         agent = mc_agent::type_id::create("agent", this);
+        scoreboard = mc_scoreboard::type_id::create("scoreboard", this);
     endfunction
 
     virtual function void connect_phase(uvm_phase phase);
         agent.mc_if = mc_if;
+        agent.mon.mc_port.connect(scoreboard.item_export);
     endfunction
 endclass
