ea1daeb18d7b9181b2fb6e1c912db305709a8be0
thinhngo11 <35941824+thinhngo11@users.noreply.github.com>
2023-05-05 16:19:17 +0700

    Update comparator_uvmtb_if.sv

--- 4bitcomparator/comparator_uvmtb_if.sv
+++ 4bitcomparator/comparator_uvmtb_if.sv
@@ -4,4 +4,16 @@ interface magnitude_comparator_if(input logic clk);
     logic A_greater;
     logic A_equal;
     logic A_less;
-endinterface
+
+    // Assertion properties
+    property p_A_greater;
+        @(posedge clk) (A_greater |=> (A > B));
+    endproperty
+
+    property p_A_equal;
+        @(posedge clk) (A_equal |=> (A == B));
+    endproperty
+
+    property p_A_less;
+        @(posedge clk) (A_less |=> (A < B));
+    endproperty
