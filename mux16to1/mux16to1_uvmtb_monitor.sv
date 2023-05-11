7adacfd1ac4e8842dd430ae9acc247b376e76df3
thinhngo11 <35941824+thinhngo11@users.noreply.github.com>
2023-05-11 07:31:16 +0700

    Update mux16to1_uvmtb_monitor.sv

--- mux16to1/mux16to1_uvmtb_monitor.sv
+++ mux16to1/mux16to1_uvmtb_monitor.sv
@@ -3,12 +3,30 @@ class mux_monitor extends uvm_monitor;
   mux_if intf;
   uvm_analysis_port #(mux_transaction) ap;
 
+  covergroup mux_cg(string name) @(posedge intf.clk);
+    option.per_instance = 1;
+    option.comment = name;
+
+    coverpoint sel_cp {
+      bins sel[] = {[0:15]};
+    }
+
+    coverpoint data_in_cp {
+      bins data_in[] = {[0:65535]};
+    }
+
+    cross sel_cp, data_in_cp;
+  endgroup
+
+  mux_cg cg;
+  
   function new(string name, uvm_component parent);
     super.new(name, parent);
   endfunction
 
   virtual function void build_phase(uvm_phase phase);
     ap = new("ap", this);
+    cg = new("mux_cg");    
   endfunction
 
   virtual task run_phase(uvm_phase phase);
