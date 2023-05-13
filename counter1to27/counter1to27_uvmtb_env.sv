ec73fe5387cfd45febd8e75106b112ca8b88a23f
thinhngo11 <35941824+thinhngo11@users.noreply.github.com>
2023-05-13 14:30:21 +0700

    Update counter1to27_uvmtb_env.sv

--- counter1to27/counter1to27_uvmtb_env.sv
+++ counter1to27/counter1to27_uvmtb_env.sv
@@ -1,6 +1,7 @@
 class counter_env extends uvm_env;
     counter_agent agent;
     counter_scoreboard sb;
+    counter_coverage_collector cov_col;
 
     function new(string name, uvm_component parent);
         super.new(name, parent);
@@ -10,9 +11,16 @@ class counter_env extends uvm_env;
         super.build_phase(phase);
         agent = counter_agent::type_id::create("agent", this);
         sb = counter_scoreboard::type_id::create("sb", this);
+        cov_col = counter_coverage_collector::type_id::create("cov_col", this);
     endfunction
 
     virtual function void connect_phase(uvm_phase phase);
         sb.analysis_export.connect(agent.ap);
+        cov_col.analysis_export.connect(agent.ap);
+    endfunction
+    
+    virtual function void report_phase(uvm_phase phase);
+        super.report_phase(phase);
+        cov_col.report_coverage();
     endfunction
 endclass
