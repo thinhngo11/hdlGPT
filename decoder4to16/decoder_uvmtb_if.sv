25ceafb63ac7e7f12bd113a92d9afdc4016c1575
thinhngo11 <35941824+thinhngo11@users.noreply.github.com>
2023-05-18 12:55:11 +0700

    Delete decoder4to16 directory

--- decoder4to16/decoder_uvmtb_if.sv
+++
@@ -1,8 +0,0 @@
-interface decoder_4to16_if(input logic clk);
-    logic [3:0] sel;
-    logic [15:0] out;
-
-    modport decoder_mp(input sel, output out);
-    modport tb_mp(input out, output sel);
-
-endinterface
