85e4a8fd8cec28c7107f8e62e92434afd24be2c6
thinhngo11 <35941824+thinhngo11@users.noreply.github.com>
2023-05-18 13:03:58 +0700

    Delete FIFO directory

--- FIFO/FIFO_uvmtb_sequencer.sv
+++
@@ -1,9 +0,0 @@
-`include "fifo_pkg.sv"
-
-class fifo_sequencer extends uvm_sequencer #(fifo_transaction);
-    `uvm_component_utils(fifo_sequencer)
-
-    function new(string name, uvm_component parent);
-        super.new(name, parent);
-    endfunction
-endclass
