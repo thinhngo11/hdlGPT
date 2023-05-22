b5548d40802035f5f33231047696e0d99aef1b44
thinhngo11 <35941824+thinhngo11@users.noreply.github.com>
2023-05-22 14:14:09 +0700

    Create counter.v

---
+++ analysis/johnsoncounter/counter.v
@@ -0,0 +1,16 @@
+module JC #(parameter WIDTH=4) 
+(
+input clk;
+input rstn;
+output reg [WIDTH-1:0] out);
+
+always @(posedge clk) begin)
+  if (!rstn)
+    out <= 1;
+else begin
+    out[WIDTH-1] <= ~out[0];
+    for (int i=0; i < WIDTH-1; i=i+1) begin
+        out[i] <= out[i+1];
+    end
+end
+endmodule
